8c19bbed5de2c7562e8f24db031dc687c8d049bc
InquestGeronimo <rcosta84@gmail.com>
2023-12-21 16:24:09 -0500

    cleanup

--- src/takeoff/cloud_logs.py
+++ src/takeoff/cloud_logs.py
@@ -1,11 +1,13 @@
 import boto3
 
+#------ UNDER DEVELOPMENT
+
 # Initialize the CloudWatch Logs client
 cloudwatch = boto3.client('logs', region_name='us-east-1')  # Replace 'us-east-1' with your desired region
 
 # Specify the log group name and log stream name for your SageMaker endpoint
 log_group_name = '/aws/sagemaker/Endpoints/takeoff-endpoint'
-log_stream_name = 'AllTraffic/i-0a3687981090fe7d5'
+log_stream_name = 'AllTraffic/i-0a3687981090f'
 
 # Retrieve the CloudWatch logs
 response = cloudwatch.get_log_events(
