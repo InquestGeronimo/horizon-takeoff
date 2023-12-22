ed0170abed685c9d0e3c1b99090605c8e3d12da2
InquestGeronimo <rcosta84@gmail.com>
2023-12-22 15:54:05 -0500

    update changelog

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 NAME = "horizon"
-VERSION = "0.0.177"
+VERSION = "0.0.2"
 AUTHOR = "InquestGeronimo"
 EMAIL = "rcostanl@gmail.com"
 LD_CONTENT_TYPE = "text/markdown"
