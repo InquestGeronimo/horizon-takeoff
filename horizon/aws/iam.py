ad28088afd00c28e11863f6e24060ad27c30e6aa
InquestGeronimo <rcosta84@gmail.com>
2023-12-25 20:10:42 -0500

    add typing/docstrings

--- horizon/aws/iam.py
+++ horizon/aws/iam.py
@@ -1,16 +1,26 @@
 import boto3
 import botocore.exceptions
-
-# https://docs.aws.amazon.com/code-library/latest/ug/python_3_iam_code_examples.html
+from typing import List, Optional
 
 class IAMHandler:
+    """
+    A class for managing AWS Identity and Access Management (IAM) operations.
+    """
+
     def __init__(self):
-        # Create an IAM client
+        """
+        Initialize IAMHandler and create an IAM client.
+        """
         self.iam_client = boto3.client('iam')
 
-    def get_aws_account_id(self):
+    def get_aws_account_id(self) -> Optional[str]:
+        """
+        Get the AWS account ID associated with the IAM user.
+
+        Returns:
+            str or None: The AWS account ID or None if an error occurs.
+        """
         try:
-            # Get the AWS account ID
             response = self.iam_client.get_user()
             account_id = response['User']['Arn'].split(':')[4]
             return account_id
@@ -18,9 +28,15 @@ class IAMHandler:
             print(f"Error retrieving AWS account ID: {e}")
             return None
 
-    def attach_policy_to_role(self, role_name, policy_arn):
+    def attach_policy_to_role(self, role_name: str, policy_arn: str) -> None:
+        """
+        Attach a managed policy to an IAM role.
+
+        Args:
+            role_name (str): The name of the IAM role.
+            policy_arn (str): The Amazon Resource Name (ARN) of the policy to attach.
+        """
         try:
-            # Attach a managed policy to an IAM role
             self.iam_client.attach_role_policy(
                 RoleName=role_name,
                 PolicyArn=policy_arn
@@ -29,42 +45,51 @@ class IAMHandler:
         except botocore.exceptions.ClientError as e:
             print(f"Error attaching policy '{policy_arn}' to IAM role '{role_name}': {e}")
 
-    def create_instance_profile_and_associate_role(self, role_name):
+    def create_instance_profile_and_associate_role(self, role_name: str) -> None:
+        """
+        Create an instance profile and associate it with an IAM role.
+
+        Args:
+            role_name (str): The name of the IAM role.
+        """
         try:
-            # Create an instance profile
             self.iam_client.create_instance_profile(InstanceProfileName=role_name)
-
-            # Add the IAM role to the instance profile
             self.iam_client.add_role_to_instance_profile(InstanceProfileName=role_name, RoleName=role_name)
-
             print(f"Instance profile '{role_name}' created and associated with IAM role '{role_name}' successfully.")
         except botocore.exceptions.ClientError as e:
             print(f"Error creating instance profile and associating it with IAM role '{role_name}': {e}")
 
-    def list_roles(self, count):
+    def list_roles(self, count: int) -> List[dict]:
         """
-        Lists the specified number of roles for the account.
+        List the specified number of roles for the account.
 
-        :param count: The number of roles to list.
+        Args:
+            count (int): The number of roles to list.
+
+        Returns:
+            List[dict]: A list of role dictionaries.
         """
         try:
             roles = self.iam_client.list_roles(MaxItems=count)['Roles']
             for role in roles:
                 print("Role:", role['RoleName'])
+            return roles
         except botocore.exceptions.ClientError as e:
             print("Couldn't list roles for the account:", e)
             raise
         else:
             return roles
         
-    def list_instance_profile_arns(self):
+    def list_instance_profile_arns(self) -> List[str]:
+        """
+        List the Amazon Resource Names (ARNs) of instance profiles in the AWS account.
+
+        Returns:
+            List[str]: A list of instance profile ARNs.
+        """
         try:
-            # List instance profiles
             response = self.iam_client.list_instance_profiles()
-
-            # Extract the ARNs from the response
             instance_profile_arns = [profile["Arn"] for profile in response["InstanceProfiles"]]
-
             return instance_profile_arns
         except Exception as e:
             # Handle exceptions or errors here
